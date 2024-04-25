162f436ccf7c30087a00eec2ab1ad4268ccee7b9
Johan Hammes <hammes.jc@gmail.com>
2024-04-25 13:58:47 +0200

    Cube LUT for tonemapping

--- Source/Samples/Earthworks_4/hlsl/compute_tonemapper.hlsl
+++ Source/Samples/Earthworks_4/hlsl/compute_tonemapper.hlsl
@@ -1,5 +1,7 @@
 
-				
+
+SamplerState linearSampler : register(s0);
+
 
 Texture2D<float3>       hdr : register(u0);
 RWTexture2D<float3>     output : register(u1);
@@ -39,5 +41,9 @@ VSQuadOut vsMain(uint vId : SV_VertexID)
 
 float4 psMain(VSQuadOut vIn) : SV_TARGET0
 {
-    return float4(ACESFilm(hdr[vIn.position.xy] * 4.1), 1);
+    float4 aces = float4(ACESFilm(hdr[vIn.position.xy] * 6.1), 1);
+    float3 cc = cube.SampleLevel(linearSampler, aces.rgb * 0.95, 0);
+    //if (vIn.position.x > 1300)
+    aces.rgb = lerp(aces.rgb, cc, 0.3);
+    return aces;
 }
