2ba292130b2ec2638f083523e24abb2895bdace5
Johan Hammes <hammes.jc@gmail.com>
2024-06-21 09:49:12 +0200

    menu will menu

--- Source/Samples/Kolskoot/Kolskoot.cpp
+++ Source/Samples/Kolskoot/Kolskoot.cpp
@@ -1960,7 +1960,6 @@ void Kolskoot::onGuiMenubar(Gui* _gui)
             if (ImGui::Button("Menu", ImVec2(100, 0)))
             {
                 guiMode = gui_menu;
-                if (launchedFromBuilder) guiMode = gui_exercises;
                 zigbeeRounds(0, 0, true);		// turn all air off
             }
             TOOLTIP("'esc'");
